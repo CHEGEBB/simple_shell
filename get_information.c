ecf6af0fea43aaae5d280a276140107441491279
@BrianChege <123733116+CHEGEBB@users.noreply.github.com>
2023-11-15 15:40:14 +0300

    Update get_information.c

--- get_information.c
+++ get_information.c
@@ -1,74 +1,73 @@
-#include "shell.h"
+#include "custom_shell.h"
 
 /**
- * clear_info - initializes info_t struct
- * @info: struct address
+ * reset_data_struct - initializes data_t structure
+ * @data: structure address
  */
-void clear_info(info_t *info)
+void reset_data_struct(data_t *data)
 {
-	info->arg = NULL;
-	info->argv = NULL;
-	info->path = NULL;
-	info->argc = 0;
+	data->arg = NULL;
+	data->args = NULL;
+	data->path_loc = NULL;
+	data->arg_count = 0;
 }
 
 /**
- * set_info - initializes info_t struct
- * @info: struct address
+ * initialize_data_struct - initializes data_t structure
+ * @data: structure address
  * @av: argument vector
  */
-void set_info(info_t *info, char **av)
+void initialize_data_struct(data_t *data, char **av)
 {
-	int i = 0;
+	int index = 0;
 
-	info->fname = av[0];
-	if (info->arg)
+	data->filename = av[0];
+	if (data->arg)
 	{
-		info->argv = strtow(info->arg, " \t");
-		if (!info->argv)
+		data->args = split_string(data->arg, " \t");
+		if (!data->args)
 		{
-
-			info->argv = malloc(sizeof(char *) * 2);
-			if (info->argv)
+			data->args = malloc(sizeof(char *) * 2);
+			if (data->args)
 			{
-				info->argv[0] = _strdup(info->arg);
-				info->argv[1] = NULL;
+				data->args[0] = duplicate_string(data->arg);
+				data->args[1] = NULL;
 			}
 		}
-		for (i = 0; info->argv && info->argv[i]; i++)
+		for (index = 0; data->args && data->args[index]; index++)
 			;
-		info->argc = i;
+		data->arg_count = index;
 
-		replace_alias(info);
-		replace_vars(info);
+		alter_aliases(data);
+		alter_variables(data);
 	}
 }
 
 /**
- * free_info - frees info_t struct fields
- * @info: struct address
- * @all: true if freeing all fields
+ * release_data_struct - frees data_t structure fields
+ * @data: structure address
+ * @free_all: true if freeing all fields
  */
-void free_info(info_t *info, int all)
+void release_data_struct(data_t *data, int free_all)
 {
-	ffree(info->argv);
-	info->argv = NULL;
-	info->path = NULL;
-	if (all)
+	free_string_array(data->args);
+	data->args = NULL;
+	data->path_loc = NULL;
+	if (free_all)
 	{
-		if (!info->cmd_buf)
-			free(info->arg);
-		if (info->env)
-			free_list(&(info->env));
-		if (info->history)
-			free_list(&(info->history));
-		if (info->alias)
-			free_list(&(info->alias));
-		ffree(info->environ);
-			info->environ = NULL;
-		bfree((void **)info->cmd_buf);
-		if (info->readfd > 2)
-			close(info->readfd);
+		if (!data->cmd_buf)
+			free(data->arg);
+		if (data->env_list)
+			free_list(&(data->env_list));
+		if (data->history_list)
+			free_list(&(data->history_list));
+		if (data->alias_list)
+			free_list(&(data->alias_list));
+		free_string_array(data->env_vars);
+		data->env_vars = NULL;
+		free_buffer((void **)data->cmd_buf);
+		if (data->read_fd > 2)
+			close(data->read_fd);
 		_putchar(BUF_FLUSH);
 	}
 }
