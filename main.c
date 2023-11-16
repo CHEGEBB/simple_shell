d598148cbeae0e23f439bcb664172bd2b18d7436
CHEGEBB <chegephil24@gmail.com>
2023-11-16 17:25:51 +0300

    trial 6

--- main.c
+++ main.c
@@ -1,35 +1,34 @@
 #include "shell.h"
-
 /**
- * main - Entry point to initialize program variables.
- * @argc: Number of arguments from the command line.
- * @argv: Array of arguments from the command line.
- * @env: Environment variables.
- * Return: Zero on success.
+ * main - initialize the variables of the program
+ * @argc: number of values received from the command line
+ * @argv: values received from the command line
+ * @env: number of values received from the command line
+ * Return: zero on succes.
  */
 int main(int argc, char *argv[], char *env[])
 {
 	data_of_program data_struct = {NULL}, *data = &data_struct;
-	char *custom_prompt = "";
+	char *prompt = "";
 
-	initialize_program_data(data, argc, argv, env);
+	inicialize_data(data, argc, argv, env);
 
 	signal(SIGINT, handle_ctrl_c);
 
 	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
-	{
-		/* We are in the terminal, interactive mode */
-		errno = 2; /* Setting a custom value for clarity */
-		custom_prompt = PROMPT_MSG;
+	{/* We are in the terminal, interactive mode */
+		errno = 2;/*???????*/
+		prompt = PROMPT_MSG;
 	}
 	errno = 0;
-	sisifo(custom_prompt, data);
+	sisifo(prompt, data);
 	return (0);
 }
 
 /**
- * handle_ctrl_c - Handles the SIGINT signal (Ctrl+C).
- * @opr: Unused parameter for signal handler prototype.
+ * handle_ctrl_c - print the prompt in a new line
+ * when the signal SIGINT (ctrl + c) is send to the program
+ * @UNUSED: option of the prototype
  */
 void handle_ctrl_c(int opr UNUSED)
 {
@@ -38,21 +37,21 @@ void handle_ctrl_c(int opr UNUSED)
 }
 
 /**
- * initialize_program_data - Initializes the program's data structure.
- * @data: Pointer to the program's data structure.
- * @argv: Array of arguments passed to the program.
- * @env: Environment variables.
- * @argc: Number of values received from the command line.
+ * inicialize_data - inicialize the struct with the info of the program
+ * @data: pointer to the structure of data
+ * @argv: array of arguments pased to the program execution
+ * @env: environ pased to the program execution
+ * @argc: number of values received from the command line
  */
-void initialize_program_data(data_of_program *data, int argc, char *argv[], char **env)
+void inicialize_data(data_of_program *data, int argc, char *argv[], char **env)
 {
-	int index = 0;
+	int i = 0;
 
 	data->program_name = argv[0];
 	data->input_line = NULL;
 	data->command_name = NULL;
 	data->exec_counter = 0;
-
+	/* define the file descriptor to be readed*/
 	if (argc == 1)
 		data->file_descriptor = STDIN_FILENO;
 	else
@@ -60,37 +59,35 @@ void initialize_program_data(data_of_program *data, int argc, char *argv[], char
 		data->file_descriptor = open(argv[1], O_RDONLY);
 		if (data->file_descriptor == -1)
 		{
-			_print_error_message(data->program_name, argv[1]);
+			_printe(data->program_name);
+			_printe(": 0: Can't open ");
+			_printe(argv[1]);
+			_printe("\n");
 			exit(127);
 		}
 	}
-
 	data->tokens = NULL;
 	data->env = malloc(sizeof(char *) * 50);
-
 	if (env)
 	{
-		while (env[index])
+		for (; env[i]; i++)
 		{
-			data->env[index] = str_duplicate(env[index]);
-			index++;
+			data->env[i] = str_duplicate(env[i]);
 		}
 	}
-	data->env[index] = NULL;
+	data->env[i] = NULL;
 	env = data->env;
 
 	data->alias_list = malloc(sizeof(char *) * 20);
-
-	for (index = 0; index < 20; index++)
+	for (i = 0; i < 20; i++)
 	{
-		data->alias_list[index] = NULL;
+		data->alias_list[i] = NULL;
 	}
 }
-
 /**
- * sisifo - Infinite loop to display the prompt and handle user input.
- * @prompt: Custom prompt to be printed.
- * @data: Pointer to the program's data structure.
+ * sisifo - its a infinite loop that shows the prompt
+ * @prompt: prompt to be printed
+ * @data: its a infinite loop that shows the prompt
  */
 void sisifo(char *prompt, data_of_program *data)
 {
@@ -99,23 +96,21 @@ void sisifo(char *prompt, data_of_program *data)
 	while (++(data->exec_counter))
 	{
 		_print(prompt);
-		error_code = string_len = _custom_getline(data);
+		error_code = string_len = _getline(data);
 
 		if (error_code == EOF)
 		{
 			free_all_data(data);
-			exit(errno);
+			exit(errno); /* if EOF is the fisrt Char of string, exit*/
 		}
 		if (string_len >= 1)
 		{
 			expand_alias(data);
 			expand_variables(data);
 			tokenize(data);
-
 			if (data->tokens[0])
-			{
+			{ /* if a text is given to prompt, execute */
 				error_code = execute(data);
-
 				if (error_code != 0)
 					_print_error(error_code, data);
 			}
